ef385280f228d306fe52641c276af1d13c5dcdc5
Marco-Vitale <vitale.marco@outlook.com>
2023-06-14 16:08:35 +0200

    server: added basic api for authentication

--- server/index.js
+++ server/index.js
@@ -1,10 +1,113 @@
 'use strict';
 
 const express = require('express');
+const morgan = require('morgan');                                  // logging middleware
+const cors = require('cors');
 
-// init express
+const userDao = require('./dao-users'); // module for accessing the user table in the DB
+
+// init express and set-up the middlewares ***/
 const app = new express();
 const port = 3001;
+
+app.use(morgan('dev'));
+app.use(express.json());
+
+/** Set up and enable Cross-Origin Resource Sharing (CORS) **/
+const corsOptions = {
+  origin: 'http://localhost:5173',
+  credentials: true,
+};
+app.use(cors(corsOptions));
+
+/*** Passport ***/
+
+/** Authentication-related imports **/
+const passport = require('passport');                              // authentication middleware
+const LocalStrategy = require('passport-local');                   // authentication strategy (username and password)
+
+/** Set up authentication strategy to search in the DB a user with a matching password.
+ * The user object will contain other information extracted by the method userDao.getUser (i.e., id, username, name).
+ **/
+passport.use(new LocalStrategy(async function verify(username, password, callback) {
+  const user = await userDao.getUser(username, password)
+  if(!user)
+    return callback(null, false, 'Incorrect username or password');  
+    
+  return callback(null, user); // NOTE: user info in the session (all fields returned by userDao.getUser, i.e, id, username, name)
+}));
+
+// Serializing in the session the user object given from LocalStrategy(verify).
+passport.serializeUser(function (user, callback) { // this user is id + username + name 
+  callback(null, user);
+});
+
+// Starting from the data in the session, we extract the current (logged-in) user.
+passport.deserializeUser(function (user, callback) { // this user is id + email + name 
+  // if needed, we can do extra check here (e.g., double check that the user is still in the database, etc.)
+  // e.g.: return userDao.getUserById(id).then(user => callback(null, user)).catch(err => callback(err, null));
+
+  return callback(null, user); // this will be available in req.user
+});
+
+/** Creating the session */
+const session = require('express-session');
+
+app.use(session({
+  secret: "shhhhh... it's a secret!",
+  resave: false,
+  saveUninitialized: false,
+}));
+app.use(passport.authenticate('session'));
+
+/** Defining authentication verification middleware **/
+const isLoggedIn = (req, res, next) => {
+  if(req.isAuthenticated()) {
+    return next();
+  }
+  return res.status(401).json({error: 'Not authorized'});
+}
+
+/*** Users APIs ***/
+
+// POST /api/sessions 
+// This route is used for performing login.
+app.post('/api/sessions', function(req, res, next) {
+  passport.authenticate('local', (err, user, info) => { 
+    if (err)
+      return next(err);
+      if (!user) {
+        // display wrong login messages
+        return res.status(401).json({ error: info});
+      }
+      // success, perform the login and extablish a login session
+      req.login(user, (err) => {
+        if (err)
+          return next(err);
+        
+        // req.user contains the authenticated user, we send all the user info back
+        // this is coming from userDao.getUser() in LocalStratecy Verify Fn
+        return res.json(req.user);
+      });
+  })(req, res, next);
+});
+
+// GET /api/sessions/current
+// This route checks whether the user is logged in or not.
+app.get('/api/sessions/current', (req, res) => {
+  if(req.isAuthenticated()) {
+    res.status(200).json(req.user);}
+  else
+    res.status(401).json({error: 'Not authenticated'});
+});
+
+// DELETE /api/session/current
+// This route is used for loggin out the current user.
+app.delete('/api/sessions/current', (req, res) => {
+  req.logout(() => {
+    res.status(200).json({});
+  });
+});
 
 // activate the server
 app.listen(port, () => {
